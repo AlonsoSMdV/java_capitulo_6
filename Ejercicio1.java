2353231ffafe7d6d4d0fe4ef90eda0ef79ac2991
AlonsoSMdV <asanmor011004@gmail.com>
2022-11-10 09:05:57 +0100

    New Commit

---
+++ Ejercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ejercicio1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Se mostrará la tirada de tres dados");
+        for(int i = 1; i <= 3; i++){
+            int dado = (int)(Math.random()*6+1);
+            System.out.println("Tirada "+i+": "+dado);
+            sum += dado;
+        }
+        System.out.println("La suma de las tres tiradas es: "+sum);
+        sc.close();
+    }
+}
