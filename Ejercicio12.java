21e791ffffe8d1df3a6883eb6df84c6a75c5d04d
AlonsoSMdV <asanmor011004@gmail.com>
2022-11-15 10:08:24 +0100

    New Commit

---
+++ Ejercicio12.java
@@ -0,0 +1,7 @@
+public class Ejercicio12 {
+    public static void main(String[] args){
+        for(int i = 0; i <= 2000;i++){
+            System.out.print((char)(Math.random()*95+32)+" ");
+        }
+    }
+}
