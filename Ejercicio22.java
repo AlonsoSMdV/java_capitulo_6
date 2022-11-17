442d6c28c17dd037b9d3d2dace937c369f9b06c1
AlonsoSMdV <asanmor011004@gmail.com>
2022-11-17 08:58:41 +0100

    Changes

--- Ejercicio22.java
+++ Ejercicio22.java
@@ -1,20 +1,21 @@
+import java.util.Scanner;
 public class Ejercicio22 {
     public static void main(String[] args){
-        System.out.print("Por favor, introduzca la longitud de la serpiente en ");
-        System.out.print("caracteres contando la cabeza: ");
-        int longitud = Integer.parseInt(System.console().readLine());
-        System.out.println(" @");
-        int x = 13;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Por favor, introduzca la longitud de la serpiente en caracteres contando la cabeza: ");
+        int longitud = sc.nextInt();
+        String cabeza = "@";
+        System.out.printf(" %12s\n",cabeza);
+        int cuerpo = 13;
         while (longitud > 1) {
-            // suma -1, 0 o 1 a la variable x
-            x += (int)(Math.random() * 3) - 1;
-            // pinta x - 1 espacios
-            for (int i = 1; i < x; i++) {
+            cuerpo += (int)(Math.random() * 3) - 1;
+            for (int i = 1; i < cuerpo; i++) {
                 System.out.print(" ");
             }
-            // pinta el cuerpo
+            
             System.out.println("#");
             longitud--;
         }
+        sc.close();
     } 
 }
