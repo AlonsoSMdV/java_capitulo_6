84341b937ffdb672adba467ae22c4c90459212e3
AlonsoSMdV <asanmor011004@gmail.com>
2022-11-21 17:19:22 +0100

    New Commit

---
+++ Ejercicio24.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ejercicio24 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Por favor, introduzca un número entero positivo: ");
+        long numero = sc.nextInt();
+        long n = numero;
+        int longitud = 0;
+        do{
+            numero /= 10;
+            longitud++;
+        }while(numero == 0);
+        int posicion = (int)(Math.random()*(longitud)+1);
+        System.out.print((n/(long)(Math.pow(longitud, posicion)))%10);
+        sc.close();
+    }
+}
