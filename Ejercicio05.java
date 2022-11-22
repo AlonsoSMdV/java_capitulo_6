b30e652fa8e7c8785c7f86d3f4848a0f58df9ebb
AlonsoSMdV <asanmor011004@gmail.com>
2022-11-22 18:07:52 +0100

    Change

--- Ejercicio05.java
+++ Ejercicio05.java
@@ -2,9 +2,22 @@ import java.util.Scanner;
 public class Ejercicio05 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int minimo = 100;
+        int maximo = 199;
+        int suma = 0;
+        int n;
         for(int i = 1;i <= 50;i++){
-            System.out.print((int)(Math.random()*100+100)+" ");
+            n = (int)(Math.random()*100+100);
+            System.out.print(n+" ");
+            suma += n;
+            if(n<minimo){
+                minimo = n;
+            }
+            if(n>maximo){
+                maximo = n;
+            }
         }
+        System.out.println("\nMínimo: " + minimo + "\nMáximo: " + maximo + "\nMedia: " + suma / 50);
         sc.close();
     }
 }
